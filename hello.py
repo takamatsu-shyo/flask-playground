9b6787c8cf19e0021afb4927fd496c598c1c0c29
takamatsu-shyo <takamatsu.shyo@gmail.com>
2019-06-02 22:24:03 +0300

    Init

---
+++ hello.py
@@ -0,0 +1,33 @@
+from flask import Flask
+from flask import request
+from flask import make_response
+from flask import abort
+
+app = Flask(__name__)
+
+@app.before_first_request
+def init():
+    print('app started!')
+
+@app.route('/hello_world')
+def hello_workd():
+    return 'Hello, world!!??'
+
+@app.route('/bad')
+def bad():
+    return '<h1>Bad request...</h1>',400
+
+@app.route('/cookie')
+def cookie():
+    response = make_response('<h1>This doc carries a cookie</h1>')
+    response.set_cookie('answer','0')
+    return response
+
+@app.route('/abort')
+def myAborA():
+    abort(401)
+
+@app.route('/')
+def index():
+    user_agent = request.headers.get('User-Agent')
+    return '<p> Your browser is {}</p>'.format(user_agent)
