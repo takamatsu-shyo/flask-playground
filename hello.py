650c8368ab3085ba94638a0d8076e09a4eb26d4c
takamatsu-shyo <takamatsu.shyo@gmail.com>
2019-06-04 23:55:10 +0300

    Template 2

--- hello.py
+++ hello.py
@@ -3,8 +3,10 @@ from flask import request
 from flask import make_response
 from flask import abort
 from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
 
 @app.before_first_request
 def init():
@@ -36,3 +38,7 @@ def index():
 @app.route('/user/<name>')
 def user(name):
     return render_template('user.html', name=name)
+
+@app.errorhandler(404)
+def page_not_found(e):
+	return render_template('404.html'),404
